bad8840280268bfb423970801c06e566cd423989
dima7665 <dima7685@gmail.com>
2023-06-04 19:55:59 +0300

    Added carousel

--- src/components/landing/landing-features/landing-features-more.svelte
+++ src/components/landing/landing-features/landing-features-more.svelte
@@ -1,59 +1,81 @@
-<script>
+<script lang="ts">
 	import { _ } from '../../../services/i18n';
 	import TooltipStatic from '../../shared/components/tooltip-static.svelte';
+	// import Carousel from '../../shared/components/carousel.svelte';
+	import Carousel from 'svelte-carousel';
+
+	let carousel;
+	let innerWidth: number = 1;
+	let particlesToShow = 1;
+
+	$: if (innerWidth) {
+		particlesToShow = Math.floor((innerWidth - 200) / 300);
+	}
 </script>
+
+<svelte:window bind:innerWidth />
 
 <h3 class="section-title">{$_('key-features.more')}</h3>
 
 <div class="scrollable">
 	<div class="body">
-		<div style:z-index="101" class="container">
-			<img
-				src="landing/key-features/more/editor.png"
-				alt={$_('more-features.editor.alt')}
-				loading="lazy"
-			/>
+		<Carousel dots={false} {particlesToShow}>
+			<div class="outer-container">
+				<div style:z-index="101" class="inner-container">
+					<img
+						src="landing/key-features/more/editor.png"
+						alt={$_('more-features.editor.alt')}
+						loading="lazy"
+					/>
 
-			<TooltipStatic tooltipTop={300} tooltipLeft={30}
-				>{$_('more-features.editor.description')}</TooltipStatic
-			>
-		</div>
+					<TooltipStatic tooltipTop={300} tooltipLeft={30}
+						>{$_('more-features.editor.description')}</TooltipStatic
+					>
+				</div>
+			</div>
 
-		<div class="container">
-			<img
-				src="landing/key-features/more/properties.png"
-				alt={$_('more-features.properties.alt')}
-				loading="lazy"
-			/>
+			<div class="outer-container">
+				<div class="inner-container">
+					<img
+						src="landing/key-features/more/properties.png"
+						alt={$_('more-features.properties.alt')}
+						loading="lazy"
+					/>
 
-			<TooltipStatic tooltipTop={420} tooltipLeft={0} direction="top"
-				>{$_('more-features.properties.description')}</TooltipStatic
-			>
-		</div>
+					<TooltipStatic tooltipTop={420} tooltipLeft={0} direction="top"
+						>{$_('more-features.properties.description')}</TooltipStatic
+					>
+				</div>
+			</div>
 
-		<div style:z-index="101" class="container">
-			<img
-				src="landing/key-features/more/categories.png"
-				alt={$_('more-features.categories.alt')}
-				loading="lazy"
-			/>
+			<div class="outer-container">
+				<div style:z-index="101" class="inner-container">
+					<img
+						src="landing/key-features/more/categories.png"
+						alt={$_('more-features.categories.alt')}
+						loading="lazy"
+					/>
 
-			<TooltipStatic tooltipTop={180} tooltipLeft={90}
-				>{$_('more-features.categories.description')}</TooltipStatic
-			>
-		</div>
+					<TooltipStatic tooltipTop={180} tooltipLeft={90}
+						>{$_('more-features.categories.description')}</TooltipStatic
+					>
+				</div>
+			</div>
 
-		<div class="container">
-			<img
-				src="landing/key-features/more/tags.png"
-				alt={$_('more-features.tags.alt')}
-				loading="lazy"
-			/>
+			<div class="outer-container">
+				<div class="inner-container">
+					<img
+						src="landing/key-features/more/tags.png"
+						alt={$_('more-features.tags.alt')}
+						loading="lazy"
+					/>
 
-			<TooltipStatic tooltipTop={330} tooltipLeft={70}
-				>{$_('more-features.tags.description')}</TooltipStatic
-			>
-		</div>
+					<TooltipStatic tooltipTop={330} tooltipLeft={70}
+						>{$_('more-features.tags.description')}</TooltipStatic
+					>
+				</div>
+			</div>
+		</Carousel>
 	</div>
 </div>
 
@@ -85,12 +107,21 @@
 	}
 
 	.body {
-		display: grid;
-		grid-template-columns: 1fr 1fr 1fr 1fr;
-		min-width: 1000px;
+		display: flex;
+		// display: grid;
+		// grid-template-columns: 1fr;
+		// min-width: 1000px;
 	}
 
-	.container {
+	.outer-container {
+		display: flex;
+		justify-content: center;
+		// position: relative;
+		// align-content: baseline;
+		// z-index: 10;
+	}
+
+	.inner-container {
 		display: block;
 		position: relative;
 		align-content: baseline;
